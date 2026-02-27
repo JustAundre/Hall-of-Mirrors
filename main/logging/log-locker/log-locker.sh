21de275524cd8294010996774cf7da2ef0aed37e
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-02-27 00:09:39 -0600

    Minor comment consistency change

--- main/logging/log-locker/log-locker.sh
+++ main/logging/log-locker/log-locker.sh
@@ -31,6 +31,7 @@ for file in "$logDir"/*; do
 	# Check if this specific file is in our list (or is being prepared for logging)
 	if echo "$isOpen" | grep -qx "$file" || [[ "$(stat -c %s $file)" == '0' ]]; then
 		# File is actively being logged to
+		# Add append-only (+a)
 		chattr +a "$file" && echo "Made append-only: $file"
 	else
 		# File is idle/finished
