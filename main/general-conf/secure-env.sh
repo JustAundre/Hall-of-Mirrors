30e7d0fcdd968a8e5928109bf24b05d0c6cb112b
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-02-27 21:10:48 -0600

    Minor fix relating to reduction of information leakage
    Minor refactoring
    
    Refined shell opts
    
    Fixed possible race-condition with SSH_TTY variable
    
    Made LD_PRELOAD readonly
    
    Set UID variable if not already set
    
    Made TMOUT variable modifiable

--- main/general-conf/secure-env.sh
+++ main/general-conf/secure-env.sh
@@ -1,40 +1,43 @@
-# Hide terminal feedback
-stty -echo
-#
-# Set secure shell configurations
-set +x nolog
-set -o functrace errtrace ignoreeof pipefail
-shopt -s histappend
-#
-# Remove ability to change shell configurations
-umask 0037
-enable -n set shopt umask kill builtin command getopts unalias ulimit disown enable times echo printf
-#
-# Lock down variables
-TTY=$(tty)
-declare -rx\
-	PROMPT_COMMAND='
+#!/bin/bash
+# Curly brackets to stop output of staging commands
+{
+	# Set secure/safe shell configurations
+	set +o xtrace verbose monitor nolog ignoreeof
+	set -o functrace errtrace pipefail nounset noclobber
+	shopt -s histappend histverify cmdhist dotglob globstar
+	shopt -u cdable_vars cdspell dirspell interactive_comments extglob nocaseglob nocasematch expand_aliases
+	umask 0077
+	#
+	# Disable builtin echo and printf so they're forced to make syscalls to the actual binaries
+	enable -n echo printf
+	#
+	# Remove usage of commands which allow bypasses or unauthorized forensics
+	enable -n set shopt umask kill builtin command getopts unalias ulimit disown enable times alias
+	#
+	# Lock down variables
+	TTY="$(tty)"
+	read -r PROMPT_COMMAND <<-'EOF'
 		history -a
 		history -w
+		USER="$(getent passwd $UID | cut -d: -f1)"
 		sleep 0.15
-	'\
-	LOG\
-	SSH_CONNECTION\
-	SSH_CLIENT\
-	TTY="${TTY##*/}"\
-	SSH_TTY="${TTY##*/}"\
-	SSH_ORIGINAL_COMMAND\
-	HOSTNAME="${HOSTNAME%%.*}"\
-	USER\
-	LUID\
-	LOGNAME\
-	HOME\
-	HISTIGNORE=\
-	HISTCONTROL=\
-	HISTSIZE=10000\
-	HISTFILESIZE=10000\
-	HISTFILE="$HOME/.bash_history"
-TMOUT=90
-#
-# Restore terminal feedback
-stty echo
+	EOF
+	TMOUT=90
+	declare -rx\
+		SSH_CONNECTION\
+		SSH_CLIENT\
+		SSH_TTY="${TTY##*/}"\
+		TTY="${TTY##*/}"\
+		SSH_ORIGINAL_COMMAND\
+		HOSTNAME="${HOSTNAME%%.*}"\
+		LOGNAME\
+		HOME\
+		HISTIGNORE=\
+		HISTCONTROL=\
+		LD_PRELOAD=\
+		HISTSIZE=10000\
+		HISTFILESIZE=10000\
+		HISTFILE="$HOME/.bash_history"\
+		UID="$(/usr/bin/cat /proc/self/loginuid)"\
+		PROMPT_COMMAND
+} >/dev/null
