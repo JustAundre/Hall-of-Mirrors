9a54f85608686bc6b2e18d71923b9c9c5066e132
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-05-08 14:49:18 -0500

    Expanded "TUI Firewall Configuration" stub in `server/hardening/firewall.sh`.
    Added `exit 0` to the end of the `success` function in `server/hardening/.allrc`.

--- server/hardening/firewall.sh
+++ server/hardening/firewall.sh
@@ -58,4 +58,74 @@ fi
 #
 # TUI Firewall Configuration
 #
-# WIP
+# (this while true; is too small to warrant indentation)
+confirm 'Enter TUI firewall configuration' && until [[ -n "${exit}" ]]; do
+# A way to exit the loop
+trap $'
+	exit=true
+	echo "W: You\'ve hit [CTRL] + C or sent SIGINT; closing after this run finishes..."
+' SIGINT
+echo 'i: Hit [CTRL] + C/send SIGINT on your last run to exit TUI firewall configuration.'
+#
+# Available options:
+# Persistence state
+# Action types (FirewallD can block ICMP-type requests)
+persistences=(
+	'On-disk'
+	'In-memory'
+	'On-disk & in-memory'
+)
+directions=(
+	'Incoming traffic'
+	'Outgoing traffic'
+)
+action_types=(
+	'Allow'
+	'Drop'
+	'Reject'
+)
+classes=(
+	'ICMP type'
+	'IP'
+	'Port'
+	'IP:port'
+)
+hash firewall-cmd && action_types+=('Disallow ICMP type')
+#
+# Get the persistence type of the rule
+# FirewallD has options
+# ...and UFW doesn't.
+if hash firewall-cmd; then persistence="$(checklist 'Where should this rule go?' radiolist "${persistences[@]}")"
+else persistence='On-disk & in-memory'
+fi
+#
+# Get the direction of the rule
+direction="$(checklist 'Set a rule for...' radiolist "${directions[@]}")"
+#
+# Get the type of the rule
+action_type="$(checklist 'Do what with captured requests?' radiolist "${action_types[@]}")"
+#
+# Get the class of the target requests
+class="$(checklist "What are we ${action_type,,}ing?" radiolist "${classes[@]}")"
+#
+# Check for the exit signal
+[[ -n "${exit}" ]] && continue
+#
+# Assemble the command
+if hash firewall-cmd; then
+	echo "${persistence}" | grep -q 'on-disk' && args+=('--permanent')
+	# WIP
+	echo "${persistence}" | grep -q 'in-memory' && firewall-cmd --reload
+elif hash ufw; then
+	args+=('ufw')
+fi
+done
+
+
+
+
+#
+# Exit
+#
+# Display success message and exit.
+success
