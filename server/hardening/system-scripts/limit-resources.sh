0cc75434dff551813898fbb1498cea1c37687f8c
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-05-21 00:26:47 -0500

    Minor QOL fixes
    Fixed incorrect file path(s) for `secure-env.sh`.
    
    Added stub for validating `limits.conf`.

--- server/hardening/system-scripts/limit-resources.sh
+++ server/hardening/system-scripts/limit-resources.sh
@@ -1,20 +1,44 @@
 #!/usr/bin/env bash
 ### Uses SystemD slices & /etc/security/limits.conf in tandem to help prevent 1 user from using too many system resources.
 #
-# Modify/Verify Slices
+# Modify/Verify
 #
-# Pause and warn
+# Heads up!
 echo $'Review and edit the files you\'re about to be shown as needed.'
 pause
 #
-# Verify the files
-while
+# Edit/validify slices
+(while
 	systemd-analyze verify cnf/rsc-caps/slice-individual.slice 2>&1 | grep invalid ||
 	systemd-analyze verify cnf/rsc-caps/slice-shared.slice 2>&1 | grep invalid
 do
-	[[ -z "${ran}" ]] && ran=true || echo 'W: SystemD found issue(s) with your configuration.'
-	echo $'i: After you resume, you\'ll be made to revise your configurations again.' && pause
+	if [[ -z "${ran}" ]]; then
+		ran=true
+	else
+		echo 'W: SystemD found issue(s) with your configuration.'
+		echo $'i: After you resume, you\'ll be made to revise your configurations again.'
+		pause
+	fi
 	"${EDITOR}" cnf/rsc-caps/slice-individual.conf cnf/rsc-caps/slice-shared.conf cnf/rsc-caps/limits.conf
+done)
+#
+# Edit/validify limits.conf
+until [[ "${pass}" == true ]]; do
+	"${EDITOR}" cnf/rsc-caps/limits.conf
+	while read -r 'field[0]' 'field[1]' 'field[2]' 'field[3]' 'field[4]'; do
+		# Validate the identifier field
+		if [[ "${field[0]}" =~ ^@[0-9]+(:[0-9]+)?$ || "${field[0]}" =~ ^[0-9]+(:[0-9]+)?$ || "${field[0]}" == '*' ]]; then :
+		elif [[ "${field[0]}" =~ ^[@%]([a-zA-Z_-.]{1,32})$ ]] && getent group "${BASH_REMATCH[1]}" &>/dev/null; then :
+		elif [[ "${field[0]}" =~ ^([a-zA-Z_-.]{1,32})$ ]] && getent passwd "${BASH_REMATCH[1]}" &>/dev/null; then :
+		else
+			pass=false
+			break
+		fi
+	done < <(grep -Ev '^\s*(#|$)' cnf/rsc-caps/limits.conf)
+	if [[ "${pass}" == false ]];
+	then echo 'W: There was a syntax error in your limits.conf.'
+	else pass=true
+	fi
 done
 
 
@@ -28,7 +52,7 @@ done
 install -m 640 -o 0 -g 0 cnf/rsc-caps/slice-individual.slice /etc/systemd/system/user.slice.d/override.conf
 install -m 640 -o 0 -g 0 cnf/rsc-caps/slice-shared.slice /etc/systemd/system/user-.slice.d/override.conf
 install -m 644 -o 0 -g 0 cnf/rsc-caps/limits.conf /etc/security/limits.conf
-install -m 644 -o 0 -g 0 cnf/secure-env.sh /etc/profile.d/secure-env.sh
+install -m 644 -o 0 -g 0 cnf/profile.d/secure-env.sh /etc/profile.d/secure-env.sh
 #
 # Reload SystemD
 systemctl daemon-reload
