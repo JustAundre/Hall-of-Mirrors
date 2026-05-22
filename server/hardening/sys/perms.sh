a085368c65bf753c8d3501c1264a4e7cf02ed6e3
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-05-21 19:29:51 -0500

    Executed minor format revision.

--- server/hardening/sys/perms.sh
+++ server/hardening/sys/perms.sh
@@ -142,9 +142,8 @@ find /etc/profile /etc/bashrc /etc/bash.bashrc /etc/profile.d/ -type d -exec per
 #
 # Secure local home directories
 # (including root's home @ /root)
-# Iterate over all interactive users'...
+# Iterate over all interactive users' home directories
 (for user in "${int_users[@]}"; do
-	# Home directories.
 	home="$(grep "^${user}:" | tail -n1 | cut -d: -f6)"
 	find "${home}" -type f -exec perm_fix -m 600 -o "${user}" -g "${user}" {} +
 	find "${home}" -type d -exec perm_fix -m 700 -o "${user}" -g "${user}" {} +
