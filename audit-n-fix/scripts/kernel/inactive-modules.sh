fd33522dbd4288aa065d75de95809869951efd84
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-08-01 19:45:38 -0500

    Fixed subshell redirection redirecting into `uniq -u` instead of `sort`.

--- audit-n-fix/scripts/kernel/inactive-modules.sh
+++ audit-n-fix/scripts/kernel/inactive-modules.sh
@@ -6,9 +6,9 @@ confirm 'Prevent currently unused kernel modules from ever being loaded' && whil
 	echo "install ${mod} /bin/false" >>/etc/modprobe.d/hardening.conf
 #
 # uniq -u: If a line occurs more than once, all instances of that line are removed.
-done < <(sort | uniq -u < <(
+done < <(sort < <(
 	# Combine results from active modules and ALL modules.
 	find /lib/modules/"$(uname -r)" -type f -name '*.ko*' -print0 | xargs -0n1 basename | cut -d. -f1
 	grep -Eo '^[^ ]+' /proc/modules
-))
+) | uniq -u)
 log i 'You can find blocked kernel modules @ "/etc/modprobe.d/hardening.conf"'
