cc93735bca81ba243133d0c3a93e4ce3f3a10196
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-08-01 19:42:51 -0500

    Moved from find `-exec`s to native Bash for loops.

--- audit-n-fix/scripts/cron/atd.sh
+++ audit-n-fix/scripts/cron/atd.sh
@@ -6,13 +6,19 @@
 if hash atd; then
 	while read -r path; do
 		[[ -d "${path}" && -x "${path}" ]] && atd_dir="${path}" || break
-		find "${atd_dir}" ! -name '.SEQ' -type f \
-			-exec log i "Reviewing AtD job file \"{}\"..." \; \
-			-exec sleep 2.5 \; \
-			-exec "${EDITOR}" {} \; \
-			-exec rm -vi {} \; \
-			</dev/tty
-		break
+		if [[ -d "${path}" ]]; then
+			log i "Successfully located AtD job directory @ \"${path}\""
+			mapfile -td '' atd_jobs < <(find "${atd_dir}" ! -name '.SEQ' -type f)
+		else
+			log w "Possible AtD job directory \"${path}\" doesn't exist; attempting to find another possible AtD job directory..."
+			continue
+		fi
+		for atd_job in "${atd_jobs[@]}"; do
+			log i "Reviewing AtD job file \"${atd_job}\"..."
+			sleep 2.5
+			"${EDITOR}" "${atd_job}"
+			rm -vi "${atd_dir}"
+		done
 	done < <(strings "$(which atd)" | grep -Eo '/var/spool/.+')
 	[[ -z "${atd_dir}" ]] && log w $'AtD is installed, but the directory where AtD jobs are stored couldn\'t be located.'
 fi
