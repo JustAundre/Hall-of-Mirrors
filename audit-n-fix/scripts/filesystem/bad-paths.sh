635d3d1af395099cc93663e7067a3c68af4fde56
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-08-02 01:58:29 -0500

    Minor formatting fixes.

--- audit-n-fix/scripts/filesystem/bad-paths.sh
+++ audit-n-fix/scripts/filesystem/bad-paths.sh
@@ -1,19 +1,33 @@
-# Review nodes containing non-ASCII characters
+#
+# Non-ASCII characters in Node Names
+#
 mapfile -td '' has_non_ascii < <(
-	find / -xephem -mindepth 1 ! -iregex '^[\x00-\x7F\n]+$' -print0 |
+	find / -mindepth 1 ! -iregex '^[\x00-\x7F\n]+$' -xephem -print0 |
 		tee "non-ascii-paths-${session_id}.log"
 )
-for node in "${has_non_ascii[@]}"; do
+if [[ "${#has_non_ascii[@]}" -ge 1 ]]; then
+	log i 'Be prepared to review nodes which contain non-ASCII characters in their names.'
 	pause
+fi
+for node in "${has_non_ascii[@]}"; do
 	select_fix "${node}"
 done
+
+
+
+
+
 #
-# Review nodes leading with a hyphen
+# Node Names Leading With A Hyphen
+#
 mapfile -td '' leading_hyphen < <(
-	find / -xephem -mindepth 1 -name '-*' -print0 |
+	find / -mindepth 1 -name '-*' -xephem -print0 |
 		tee "leading-hyphen-${session_id}.log"
 )
-for node in "${leading_hyphen[@]}"; do
+if [[ "${#leading_hyphen[@]}" -ge 1 ]]; then
+	log i 'Be prepared to review nodes whose name leads with a hyphen.'
 	pause
+fi
+for node in "${leading_hyphen[@]}"; do
 	select_fix "${node}"
 done
