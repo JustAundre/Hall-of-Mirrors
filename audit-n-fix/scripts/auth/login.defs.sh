fd1004d03e6d398ad816c4dcaa209d9bc61c9f89
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-08-01 19:41:30 -0500

    Fixed incorrect usage of `reconfig` lib function.

--- audit-n-fix/scripts/auth/login.defs.sh
+++ audit-n-fix/scripts/auth/login.defs.sh
@@ -2,13 +2,10 @@
 # login.defs
 #
 # Configure secure defaults fore login.defs
-(
-	declare -x target_file=/etc/login.defs
-	reconfig PASS_MAX_DAYS 90
-	reconfig PASS_MIN_DAYS 7
-	reconfig PASS_WARN_AGE 14
-	reconfig ENCRYPT_METHOD YESCRYPT
-	reconfig UMASK 077
-	reconfig CREATE_HOME yes
-	reconfig USERGROUPS_ENAB yes
-)
+reconfig PASS_MAX_DAYS 90 /etc/login.defs
+reconfig PASS_MIN_DAYS 7 /etc/login.defs
+reconfig PASS_WARN_AGE 14 /etc/login.defs
+reconfig ENCRYPT_METHOD YESCRYPT /etc/login.defs
+reconfig UMASK 077 /etc/login.defs
+reconfig CREATE_HOME yes /etc/login.defs
+reconfig USERGROUPS_ENAB yes /etc/login.defs
