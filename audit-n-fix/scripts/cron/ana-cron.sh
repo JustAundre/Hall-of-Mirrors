a749a45336ef392994b8d1608f2332e8ecafef58
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-08-01 19:42:21 -0500

    Added verbosity and separated queueing and actual removal.

--- audit-n-fix/scripts/cron/ana-cron.sh
+++ audit-n-fix/scripts/cron/ana-cron.sh
@@ -1,10 +1,25 @@
-#
-# (Ana)Cron files
-#
-# Check /etc/cron* files
-hash crond && find /etc/cron* /var/spool/anacron/cron* /etc/anacrontab -type f \
-	-exec log i 'Reviewing cron file "{}"...' \; \
-	-exec sleep 2.5 \; \
-	-exec "${EDITOR}" {} \; \
-	-exec rm -vi {} \; \
-	</dev/tty
+mapfile -td '' cron_files < <(find /etc/cron* /var/spool/anacron/cron* /etc/anacrontab -type f)
+
+if [[ "${#cron_files[@]}" -ge 1 ]]; then
+	for task in "${cron_files[@]}"; do
+		log i "Reviewing scheduled task found @ \"${task}\"..."
+		sleep 2.5
+
+		"${EDITOR}" "${task}"
+
+		if confirm "Enqueue \"${task}\" for removal"; then
+			delete_queue+=("${task}")
+			log i "\"${task}\" was queued for removal."
+		else
+			log w "\"${task}\" was not queued for removal."
+		fi
+	done
+
+	if rm -v "${delete_queue[@]}"; then
+		log i 'Removed all selected scheduled tasks.'
+	else
+		log e 'Something went wrong during the deletion.'
+	fi
+else
+	log e 'No scheduled tasks were detected and this script is now exiting...'
+fi
