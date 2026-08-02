3d0727419af037730de7445a365702cd57de8a25
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-08-01 19:46:40 -0500

    Minor refactoring.

--- audit-n-fix/scripts/kernel/systemd.sh
+++ audit-n-fix/scripts/kernel/systemd.sh
@@ -2,5 +2,5 @@
 # SystemD Configuration
 #
 # Disable core dumping
-reconfig -d \= Storage none /etc/systemd/coredump.conf
-reconfig -d \= ProcessSizeMax 0 /etc/systemd/coredump.conf
+reconfig -d'=' Storage none /etc/systemd/coredump.conf
+reconfig -d'=' ProcessSizeMax 0 /etc/systemd/coredump.conf
