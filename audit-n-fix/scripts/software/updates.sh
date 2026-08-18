3e63dd6790dd6a8b97c22ea893c21f3bdcefc514
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-08-18 00:46:11 -0500

    Formatting: add missing trailing newline @ EOFs, and fix indentation for an `if` statement.

--- audit-n-fix/scripts/software/updates.sh
+++ audit-n-fix/scripts/software/updates.sh
@@ -2,9 +2,12 @@
 # Software Updates
 #
 # Run updates for all major package distrobuters.
-if hash apt-get; then apt-get update && apt-get full-upgrade --no-install-recommends -y
-elif hash pacman; then pacman -Syuu
-elif hash dnf; then dnf upgrade --refresh
+if hash apt-get; then
+	apt-get update && apt-get full-upgrade --no-install-recommends -y
+elif hash pacman; then
+	pacman -Syuu
+elif hash dnf; then
+	dnf upgrade --refresh
 fi
 hash flatpak && flatpak update -y
 hash snap && snap refresh
